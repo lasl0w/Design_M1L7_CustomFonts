85297768ff4bc4d62f815534beae81d88177bb61
Tom Montgomery <montgomery3681@gmail.com>
2023-09-28 23:12:47 -0600

    Initial Commit

---
+++ Design_M1L7_CustomFonts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Design_M1L7_CustomFonts
+//
+//  Created by tom montgomery on 9/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Design_M1L7_CustomFonts/Design_M1L7_CustomFontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M1L7_CustomFontsApp.swift
+//  Design_M1L7_CustomFonts
+//
+//  Created by tom montgomery on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M1L7_CustomFontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
